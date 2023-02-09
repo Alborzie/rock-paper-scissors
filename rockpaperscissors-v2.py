8e06a6f56276b8c7d5dab104d5ee6622fbf9e892
Alborzie <121189444+Alborzie@users.noreply.github.com>
2023-02-09 20:35:00 +0330

    Update rockpaperscissors-v2.py

--- rockpaperscissors-v2.py
+++ rockpaperscissors-v2.py
@@ -4,37 +4,58 @@ import tkinter as tk
 plvalue = ""
 values = ["rock", "paper", "scissors"]
 cmp_value = ""
+plscore = 0
+cmpscore = 0
+drawed_times = 0
 
 def game(player, cmp):
+    global plscore
+    global cmpscore
+    global drawed_times
+
     if player == cmp:
         lblconfig(lbl_winner, "draw!")
+        drawed_times = drawed_times + 1
+        lblconfig(draw_lbl, str(drawed_times))
         return
     
     if player == "rock":
         if cmp == "paper":
             lblconfig(lbl_winner, "computer wins")
+            cmpscore = cmpscore + 1
+            lblconfig(cmp_score_lbl, str(cmpscore))
             return
         
         if cmp == "scissors":
             lblconfig(lbl_winner, "player wins")
+            plscore = plscore + 1
+            lblconfig(player_score_lbl, str(plscore))
             return
         
     if player == "paper":
         if cmp == "scissors":
             lblconfig(lbl_winner, "computer wins")
+            cmpscore = cmpscore + 1
+            lblconfig(cmp_score_lbl, str(cmpscore))
             return
         
         if cmp == "rock":
             lblconfig(lbl_winner, "player wins")
+            plscore = plscore + 1
+            lblconfig(player_score_lbl, str(plscore))
             return
     
     if player == "scissors":
         if cmp == "rock":
             lblconfig(lbl_winner, "computer wins")
+            cmpscore = cmpscore + 1
+            lblconfig(cmp_score_lbl, str(cmpscore))
             return
         
         if cmp == "paper":
             lblconfig(lbl_winner, "player wins")
+            plscore = plscore + 1
+            lblconfig(player_score_lbl, str(plscore))
             return
     
     
@@ -86,25 +107,25 @@ win.title('R,P,S')
 lbl_player = tk.Label(win, text="avalable choices:")
 lbl_player.pack()
 
-######################## create frame and player buttons ######################
+######################## create btn_frame and player buttons ######################
 rock_icon = tk.PhotoImage(file='rock.png')
 paper_icon = tk.PhotoImage(file='paper.png')
 scissors_icon = tk.PhotoImage(file='scissors.png')
 
-frame = tk.Frame(win)
-frame.columnconfigure(0, weight=1)
+btn_frame = tk.Frame(win)
+btn_frame.columnconfigure(0, weight=1)
 
-plrock = tk.Button(frame,image=rock_icon, padx=10, pady=10, command=plrock)
-plpaper = tk.Button(frame, image=paper_icon, padx=10, pady=10, command=plpaper)
-plscissors = tk.Button(frame, image=scissors_icon, padx=10, pady=10, command=plscissors)
-plrandom = tk.Button(frame, text="random choice",image=main_icon, padx=10, pady=10, compound="bottom", command=plrandom)
+plrock = tk.Button(btn_frame,image=rock_icon, padx=10, pady=10, command=plrock)
+plpaper = tk.Button(btn_frame, image=paper_icon, padx=10, pady=10, command=plpaper)
+plscissors = tk.Button(btn_frame, image=scissors_icon, padx=10, pady=10, command=plscissors)
+plrandom = tk.Button(btn_frame, text="random choice",image=main_icon, padx=10, pady=10, compound="bottom", command=plrandom)
 
 plrock.grid(row=0, column=0)
 plpaper.grid(row=0, column=1)
 plscissors.grid(row=0, column=2)
 plrandom.grid(row=0, column=3)
 
-frame.pack()
+btn_frame.pack()
 ###############################################################################
 lbl_players_choice = tk.Label(win, text="__")
 lbl_players_choice.pack()
@@ -117,6 +138,24 @@ cmp_choice.pack()
 
 lbl_winner = tk.Label(win, text="__")
 lbl_winner.pack()
+######======making player and computers score holder btn_frame =======#######
+score_frame = tk.Frame(win)
+
+player_score_msg_lbl = tk.Label(score_frame, text="player's score:")
+player_score_lbl = tk.Label(score_frame, text=str(plscore))
+cmp_score_msg_lbl = tk.Label(score_frame, text="computer's score:")
+cmp_score_lbl = tk.Label(score_frame, text=str(cmpscore))
+draw_msg_lbl = tk.Label(score_frame, text="drawed rounds:")
+draw_lbl = tk.Label(score_frame, text=str(drawed_times))
+
+player_score_msg_lbl.grid(column=0, row=0)
+player_score_lbl.grid(column=1, row=0)
+cmp_score_msg_lbl.grid(column=0, row=1)
+cmp_score_lbl.grid(column=1, row=1)
+draw_msg_lbl.grid(column=0, row=2)
+draw_lbl.grid(column=1, row=2)
+
+score_frame.pack(pady=20)
 
 win.mainloop()
 ###############################################################################
